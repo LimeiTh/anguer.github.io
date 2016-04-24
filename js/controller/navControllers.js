34302ae686c4364d8ac4bfd317c6d473090ab18f
git <yangyue0372@163.com>
2016-04-24 17:48:57 +0800

    Init build

--- js/controller/navControllers.js
+++ js/controller/navControllers.js
@@ -3,11 +3,21 @@
  */
 angular.module('blog.navController',['ngAnimate','ngTouch'])
 
-    .controller('NavController',function($scope,$location){
+    .controller('NavController',function($scope,$location,$timeout){
         $scope.isCollapsed = true;
 
         $scope.openNewPage = function(){
             $location.path("/test");
+        };
+
+        var currentDateTime = new Date();
+        (function tick(){
+            currentDateTime = new Date();
+            $timeout(tick,1000);
+        })();
+
+        $scope.getCurrentDateTime = function(){
+            return currentDateTime.getDateTimeString();
         }
     })
 
