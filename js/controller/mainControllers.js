c69d22575e56b5cf31085eabc6c5683112ab2063
git <yangyue0372@163.com>
2016-04-24 17:29:08 +0800

    Init build

--- js/controller/mainControllers.js
+++ js/controller/mainControllers.js
@@ -4,9 +4,7 @@
 angular.module('blog.mainController',['ngAnimate','ngTouch'])
 
     .controller('MainController',function($scope,$location){
-        $scope.openNewPage = function(){
-            $location.path("/test");
-        }
+
     })
     .controller('TestController',function($scope,$location){
         $scope.backHome = function(){
--- js/controller/navControllers.js
+++ js/controller/navControllers.js
@@ -3,8 +3,12 @@
  */
 angular.module('blog.navController',['ngAnimate','ngTouch'])
 
-    .controller('NavController',function($scope){
+    .controller('NavController',function($scope,$location){
         $scope.isCollapsed = true;
+
+        $scope.openNewPage = function(){
+            $location.path("/test");
+        }
     })
 
 
